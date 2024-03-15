0414e3703ecd4df4a71d74ccf54dcac3e1102f07
6ixela <100535524+6ixela@users.noreply.github.com>
2024-03-15 17:04:44 +0100

    Update get_frame.s

--- exam/1/get_frame/get_frame.s
+++ exam/1/get_frame/get_frame.s
@@ -1,15 +1,12 @@
 .global get_frame
 
 get_frame:
-	movq %rdi, %rsi
-.loop_find:
-	addq $8, %rsp
-	cmp $0, %rdi
-	jne .loop_find
-	leaq (%rsp), %rax
-.loop_recov:
-	subq $8, %rsp
-	cmp $0, %rsi
-	jne .loop_recov
+	movq %rbp, %rax
+	testl %edi,%edi
+	je .done
+.loop:
+	movq (%rax), %rax
+	dec %edi
+	jnz .loop
 .done:
 	ret
